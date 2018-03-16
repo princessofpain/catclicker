83ac03901a7ebc4d6aed87c660880d43444096a4
Chilja Speransky <magiclanzelot@gmx.net>
2018-03-16 14:14:46 +0100

    requirements 2 implemented, double cat clicker

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,19 +1,56 @@
-const catPic = document.querySelector('.cat-pic');
+const catPic1 = document.querySelector('.cat-pic1');
+const catPic2 = document.querySelector('.cat-pic2');
 const counter = document.querySelector('.counter');
 const button = document.querySelector('.reset');
-let clickCounter = 0;
+let clickCounterIan = 0;
+let clickCounterRocket = 0;
 
-catPic.addEventListener('click', function () {
-	clickCounter++;
+const name1 = document.createElement('p');
+name1.textContent = 'My name is Ian';
+catPic1.appendChild(name1);
 
-	if (clickCounter === 1) {
-		counter.textContent = `${clickCounter} time`;
-	} else {
-		counter.textContent = `${clickCounter} times`;
+const name2 = document.createElement('p');
+name2.textContent = 'My name is Rocket';
+catPic2.appendChild(name2);
+
+catPic1.addEventListener('click', function () {
+	clickCounterIan++;
+
+	if (clickCounterIan === 1 && clickCounterRocket === 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} time
+		and Rocket ${clickCounterRocket} time`;
+	} else if (clickCounterIan === 1 && clickCounterRocket != 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} time
+		and Rocket ${clickCounterRocket} times`;
+	} else if (clickCounterIan != 1 && clickCounterRocket === 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} times
+		and Rocket ${clickCounterRocket} time`;
+	} else if (clickCounterIan != 1 && clickCounterRocket != 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} times
+		and Rocket ${clickCounterRocket} times`;
+	}
+});
+
+catPic2.addEventListener('click', function () {
+	clickCounterRocket++;
+
+	if (clickCounterIan === 1 && clickCounterRocket === 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} time
+		and Rocket ${clickCounterRocket} time`;
+	} else if (clickCounterIan === 1 && clickCounterRocket != 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} time
+		and Rocket ${clickCounterRocket} times`;
+	} else if (clickCounterIan != 1 && clickCounterRocket === 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} times
+		and Rocket ${clickCounterRocket} time`;
+	} else if (clickCounterIan != 1 && clickCounterRocket != 1) {
+		counter.textContent = `You clicked Ian ${clickCounterIan} times
+		and Rocket ${clickCounterRocket} times`;
 	}
 });
 
 button.addEventListener('click', function () {
-	clickCounter = 0;
-	counter.textContent = `${clickCounter} times`;
+	clickCounterIan = 0;
+	clickCounterRocket = 0;
+	counter.textContent = 'You clicked Ian and Rocket 0 times';
 });
