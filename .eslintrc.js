4430463af217d59138dbd41a2217ae9c514016f1
Chilja Speransky <magiclanzelot@gmx.net>
2018-03-16 11:53:28 +0100

    initial set up, no gulp

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+};
