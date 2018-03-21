89e8dd308520991ce9b03772fbd476fe5e448d61
Chilja Speransky <magiclanzelot@gmx.net>
2018-03-21 11:06:24 +0100

    cat clicker premium mvc applied
    functionality given besides the counter + updating the counter

--- js/app.js
+++ js/app.js
@@ -4,11 +4,8 @@ $(function() {
 
 	// ========================== Model ============================= //
 	// store all the data like in a database, without functions
-	const model = {
+	var model = {
 		init: function() {
-
-			// array for storing unnamed objects based on Cat
-			const catList = [];
 
 			// prototype object for creating individual cat objects
 			const Cat = function(name, url, alt, clicks){
@@ -16,61 +13,62 @@ $(function() {
 				this.url = url;
 				this.alt = alt;
 				this.clicks = clicks;
-
-				catList.push(name);
 			};
 
 			// 5 new cat objects including name, url and alt text
-			new Cat('Ian','https://i.ytimg.com/vi/E9U9xS4thxU/hqdefault.jpg', 'cat in a shark', 0);
-			new Cat('Rocket', 'http://www.veryfunnycatsvideos.com/wp-content/uploads/2016/08/funny-cat-and-dog-dancing-and-si.jpg',
-				'cat singing', 0);
-			new Cat('Lily', 'https://static.boredpanda.com/blog/wp-content/uploads/2014/03/cat-burger-bed-maru-6.jpg',
-				'cat burger', 0);
-			new Cat('Sammy', 'https://i0.wp.com/justcatvideos.co/wp-content/uploads/2017/12/The-Most-Cute-and-Funny-Cat-Videos-Compilation-The-best-cat-videos-week.jpg?w=1170',
-				'cat puts the head on a table', 0);
-			new Cat('Joel', 'https://welovecatsandkittens.com/wp-content/uploads/2015/10/box-5.jpg',
-				'cat in a box', 0);
+			const cat1 = new Cat('Ian','https://i.ytimg.com/vi/E9U9xS4thxU/hqdefault.jpg', 'cat in a shark', 0);
+			const cat2 = new Cat('Rocket', 'http://www.veryfunnycatsvideos.com/wp-content/uploads/2016/08/funny-cat-and-dog-dancing-and-si.jpg',
+				'cat singing');
+			const cat3 = new Cat('Lily', 'https://static.boredpanda.com/blog/wp-content/uploads/2014/03/cat-burger-bed-maru-6.jpg',
+				'cat burger');
+			const cat4 = new Cat('Sammy', 'https://i0.wp.com/justcatvideos.co/wp-content/uploads/2017/12/The-Most-Cute-and-Funny-Cat-Videos-Compilation-The-best-cat-videos-week.jpg?w=1170',
+				'cat puts the head on a table');
+			const cat5 = new Cat('Joel', 'https://welovecatsandkittens.com/wp-content/uploads/2015/10/box-5.jpg',
+				'cat in a box');
+
+			const catList = [cat1, cat2, cat3, cat4, cat5]; // eslint-disable-line no-unused-vars
 
 			return catList;
 		},
+
+		// setClicks: function() {
+		// 	const clicks = [0, 0, 0, 0, 0];
+		// 	return clicks;
+		// },
+
+		// newClicks: function(newClicks) {
+		// 	model.setClicks().clicks = newClicks;
+		// }
 	};
 
 
 	// ========================== Controller ============================= //
 	// connect the model (like a database) and the view(the gui) through functions here
-	const octopus = {
+	var octopus = {
 
 		init() {
 			model.init();
-			view.init();
 			view.renderList();
+			// view.countClicks();
 		},
 
 
-		getAllCats: function() {
-			model.init().catList;
+		getCatArray: function() {
+			return model.init();
 		},
 
-		addNewPicture: function() {
-			view.init();
-		},
-
-		// reset: function() {
-		// 	$('.reset').addEventListener('click', function() {
-		// 		$('.counter').text(`You have clicked ${catName} 0 times`);
-		// 	});
+		// changeCatArray: function(newCatArray) {
+		// 	model.newClicks(newCatArray);
 		// }
 	};
 
 
 	// ========================== View ============================= //
-	const view = {
-		init: function() {
-		},
+	var view = {
 
 		removePicture: function() {
 			$('div.show-cat').children().remove();
-			$('.click-notification').children().remove();
+			$('.counter').text('');
 		},
 
 		// resetPicture: function() {
@@ -80,53 +78,39 @@ $(function() {
 		// },
 
 		renderList: function() {
-			$('.list-of-cats').append('<ul class="cat-ul"></ul>');
 
-			const catArray = octopus.getAllCats();
+			const catArray = octopus.getCatArray();
 
 			for(let i = 0; i < catArray.length; i++){
-				// create a new list element for every cat and add the name of the cat as textContent
-				$('.cat-ul').append(`<li>${catArray[i]}</li>`);
+			// create a new list element for every cat and add the name of the cat as textContent
+				$('.cat-ul').append(`<li id='${[i]}'>${catArray[i].name}</li>`);
+				const catLi = $(`#${[i]}`);
+				const pic = `<img src='${catArray[i].url}' alt='${catArray[i].alt}' id='${catArray[i].name}'>`;
 
-				// eventListener for click on a list element
-				$('.cat-ul').children().addEventListener('click', (function(cat) {
+				//eventListener for click on a list element
+				catLi.click((function(picCopy) {
 					return function() {
 						view.removePicture();
 
 						// add the new cat picture to the DOM and reset the counter
-						$('div.show-cat').append(cat);
-						$('.click-notification').text(`You clicked ${cat} ${cat.clicks} time/s`);
+						$('div.show-cat').append(picCopy);
+						$('.counter').text(`You clicked ${$('img').attr('id')} ${octopus.counter} time/s`);
 					};
-				})(catArray));
+				})(pic));
 			}
 		},
 
-		renderPicture: function() {
-			const pic = $('<img/>');
+		// countClicks: function() {
+		// 	const catArray = octopus.getCatArray();
 
-			const catArray = octopus.getAllCats;
-
-			for(let i = 0; i < catArray.length; i++){
-
-				// create img element and set the properties provided by the object (through the array)
-				pic.attr('src', catArray[i].url);
-				pic.attr('alt', catArray[i].alt);
-				pic.attr('id', catArray[i].name);
-
-				$('.click-notification').append(`<p class="counter">You have clicked ${catArray[i].name} 0 times</p>`);
-				$('.click-notification').append('<button class="reset">Reset</button>');
-
-				// eventListener for a click on a picture for changing the counter
-				pic.addEventListener('click', function() {
-					catArray.clicks++;
-
-					if(catArray.clicks === 1) {
-						$('.counter').text(`You have clicked ${catArray[i].name} ${catArray.clicks} time.`);
-					} else {
-						$('.counter').text(`You have clicked ${catArray[i].name} ${catArray.clicks} times.`);
-					}
-				});
-			}
-		}
+		// 	$('.showcat:first-child').click(function() {
+		// 		console.log('!');
+		// 		const index = $(`${$('img').attr('id')}`);
+		// 		catArray[index].clicks += 1;
+		// 		$('.counter').text(`You clicked ${$('img').attr('id')} ${catArray[index].clicks} time/s`);
+		// 		octopus.changeCatArray(catArray);
+		// 	});
+		// },
 	};
+	octopus.init();
 });
